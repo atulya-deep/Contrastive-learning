9d0dfc97a8dfafb01d80d93939d2f9f4322d0e51
ATULYA DEEP <83969166+atulya-deep@users.noreply.github.com>
2023-01-04 22:06:27 +0530

    Add files via upload

---
+++ supervised_contrastive loss.py
@@ -0,0 +1,16 @@
+class SupervisedContrastiveLoss(keras.losses.Loss):
+    def __init__(self, temperature=1, name=None):
+        super(SupervisedContrastiveLoss, self).__init__(name=name)
+        self.temperature = temperature
+
+    def __call__(self, labels, feature_vectors, sample_weight=None):
+        # Normalize feature vectors
+        feature_vectors_normalized = tf.math.l2_normalize(feature_vectors, axis=1)
+        # Compute logits
+        logits = tf.divide(
+            tf.matmul(
+                feature_vectors_normalized, tf.transpose(feature_vectors_normalized)
+            ),
+            self.temperature,
+        )
+        return tfa.losses.npairs_loss(tf.squeeze(labels), logits)
